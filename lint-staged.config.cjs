ed14c8c491d2108b56e607fa8a81092179538738
fktzl <fabinho@fworks.tech>
2025-08-10 00:24:16 -0300

    chore: switch Yarn to node-modules; add clsx; fix pre-commit

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,5 +1,5 @@
 /** @type {import('lint-staged').Config} */
 module.exports = {
-  '**/*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
-  '**/*.{json,md,yml,yaml,css,scss,html}': ['prettier --write']
+  '**/*.{js,jsx,ts,tsx}': ['eslint --fix', 'yarn prettier --write'],
+  '**/*.{json,md,yml,yaml,css,scss,html}': ['yarn prettier --write']
 };
