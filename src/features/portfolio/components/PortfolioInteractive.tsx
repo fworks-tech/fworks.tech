4fae42f6166abd6b8ce7ef5530c0c46d0a865efe
fktzl <fabinho@fworks.tech>
2025-09-08 16:14:06 -0300

    feat: refactor ArticlesFeature and PortfolioFeature to use dynamic imports for improved performance; add ArticlesInteractive and PortfolioInteractive components

---
+++ src/features/portfolio/components/PortfolioInteractive.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { useState } from 'react';
+
+import NeonCarousel from '@/components/shared/NeonCarousel';
+import NeonScrollbarContainer from '@/components/shared/NeonScrollbarContainer';
+import Card from '@/components/ui/Card';
+
+import PortfolioContentSection from './PortfolioContentSection';
+
+export default function PortfolioInteractive({
+  content
+}: {
+  content: {
+    title: string;
+    content: string;
+    image?: string;
+  }[];
+}) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const currentContent = content[currentIndex];
+
+  return (
+    <Card
+      variant="borderless"
+      className="flex h-full flex-col items-center p-12"
+      style={{ paddingTop: 0 }}
+    >
+      <NeonScrollbarContainer className="flex flex-col justify-center gap-4 text-center md:text-left">
+        <PortfolioContentSection title={currentContent?.title} content={currentContent?.content} />
+        <NeonCarousel sections={content} currentIndex={currentIndex} onSelect={setCurrentIndex} />
+      </NeonScrollbarContainer>
+    </Card>
+  );
+}
